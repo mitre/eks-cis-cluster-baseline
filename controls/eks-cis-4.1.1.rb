39a55430fa21baa3cbc80581a0dd9fc5703c853f
Shivani Karikar <karikarshivani@gmail.com>
2023-03-31 13:04:38 -0400

    Test change reverted
    Signed-off-by: Shivani Karikar <karikarshivani@gmail.com>

--- controls/eks-cis-4.1.1.rb
+++ controls/eks-cis-4.1.1.rb
@@ -53,7 +53,7 @@ the clusterrolebinding to the cluster-admin role :
 
   cluster_admin_principals = command("kubectl get clusterrolebindings cluster-admin --no-headers -o=custom-columns=':.subjects[*].name'").stdout.gsub("\r", '').split("\n")
 
-  if cluster_admin_principals?
+  if !cluster_admin_principals.empty?
     cluster_admin_principals.each do |principal|
       describe 'Cluster role bindings should restrict access to cluster-admin role' do
         subject { principal }
