a5167732557b663f268cb59c3bde3dec4f7dd10a
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-06-16 16:08:27 -0400

    Update eks-cis-4.2.7.rb

--- controls/eks-cis-4.2.7.rb
+++ controls/eks-cis-4.2.7.rb
@@ -3,30 +3,13 @@ control 'eks-cis-4.2.7' do
   desc  "Do not generally permit containers with capabilities assigned beyond
 the default set."
   desc  'rationale', "
-    Containers run with a default set of capabilities as assigned by the
-Container Runtime. Capabilities outside this set can be added to containers
-which could expose them to risks of container breakout attacks.
-
-    There should be at least one PodSecurityPolicy (PSP) defined which prevents
-containers with capabilities beyond the default set from launching.
-
-    If you need to run containers with additional capabilities, this should be
-defined in a separate PSP and you should carefully check RBAC controls to
-ensure that only limited service accounts and users are given permission to
-access that PSP.
+Containers run with a default set of capabilities as assigned by the Container Runtime. Capabilities outside this set can be added to containers which could expose them to risks of container breakout attacks.
+There should be at least one policy defined which prevents containers with capabilities beyond the default set from launching.
+If you need to run containers with additional capabilities, this should be defined in a separate policy and you should carefully check to ensure that only limited service accounts and users are given permission to use that policy.
   "
   desc  'check', "
-    Get the set of PSPs with the following command:
-
-    ```
-    kubectl get psp
-    ```
-
-    Verify that there are no PSPs present which have `allowedCapabilities` set
-to anything other than an empty array.
-  "
-  desc 'fix', "Ensure that `allowedCapabilities` is not present in PSPs for
-the cluster unless it is set to an empty array."
+List the policies in use for each namespace in the cluster, ensure that policies are present which prevent allowedCapabilities to be set to anything other than an empty array.  "
+  desc 'fix', "Ensure that allowedCapabilities is not present in policies for the cluster unless it is set to an empty array."
   impact 0.5
   tag severity: 'medium'
   tag gtitle: nil
