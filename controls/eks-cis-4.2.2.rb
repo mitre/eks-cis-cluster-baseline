acc9a0e96cdce85b5e06e121e1ac2db577ad7f90
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-06-16 15:49:04 -0400

    Update eks-cis-4.2.2.rb

--- controls/eks-cis-4.2.2.rb
+++ controls/eks-cis-4.2.2.rb
@@ -4,34 +4,19 @@ control 'eks-cis-4.2.2' do
   desc  "Do not generally permit containers to be run with the `hostPID` flag
 set to true."
   desc  'rationale', "
-    A container running in the host's PID namespace can inspect processes
-running outside the container. If the container also has access to ptrace
-capabilities this can be used to escalate privileges outside of the container.
+A container running in the host's PID namespace can inspect processes running outside 
+the container. If the container also has access to ptrace capabilities this can be used to 
+escalate privileges outside of the container.
 
-    There should be at least one PodSecurityPolicy (PSP) defined which does not
-permit containers to share the host PID namespace.
+There should be at least one admission control policy defined which does not permit 
+containers to share the host PID namespace.
 
-    If you need to run containers which require hostPID, this should be defined
-in a separate PSP and you should carefully check RBAC controls to ensure that
-only limited service accounts and users are given permission to access that PSP.
-  "
+If you need to run containers which require hostPID, this should be defined in a 
+separate policy and you should carefully check to ensure that only limited service 
+accounts and users are given permission to use that policy.  "
   desc  'check', "
-    Get the set of PSPs with the following command:
-
-    ```
-    kubectl get psp
-    ```
-
-    For each PSP, check whether privileged is enabled:
-
-    ```
-    kubectl get psp <name> -o=jsonpath='{.spec.hostPID}'
-    ```
-
-    Verify that there is at least one PSP which does not return true.
-  "
-  desc 'fix', "Create a PSP as described in the Kubernetes documentation,
-ensuring that the `.spec.hostPID` field is omitted or set to false."
+List the policies in use for each namespace in the cluster, ensure that each policy disallows the admission of hostPID containers"
+  desc 'fix', "Add policies to each namespace in the cluster which has user workloads to restrict the admission of hostPID containers."
   impact 0.5
   tag severity: 'medium'
   tag gtitle: nil
@@ -40,11 +25,12 @@ ensuring that the `.spec.hostPID` field is omitted or set to false."
   tag stig_id: nil
   tag fix_id: nil
   tag cci: nil
-  tag nist: ['AC-6 (9)', 'CM-2']
+  tag nist: ['AC-6 (9)', 'AC-6 (9)', 'AC-6 (2)']
   tag cis_level: 1
   tag cis_controls: [
     { '6' => ['5.1'] },
-    { '7' => ['5.2'] }
+    { '7' => ['4.3'] },
+    { '8' => ['5.4'] }
   ]
   tag cis_rid: '4.2.2'
 
