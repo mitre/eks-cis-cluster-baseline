5960fd2d97e36aa1e9bd5a41c9ce75491b9d1809
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-06-16 16:06:41 -0400

    Update eks-cis-4.2.5.rb

--- controls/eks-cis-4.2.5.rb
+++ controls/eks-cis-4.2.5.rb
@@ -1,39 +1,16 @@
 control 'eks-cis-4.2.5' do
   title 'Minimize the admission of containers with
   allowPrivilegeEscalation'
-  desc  "Do not generally permit containers to be run with the
-`allowPrivilegeEscalation` flag set to true."
+  desc  "Do not generally permit containers to be run with the allowPrivilegeEscalation flag set to true. Allowing this right can lead to a process running a container getting more rights than it started with.
+It's important to note that these rights are still constrained by the overall container sandbox, and this setting does not relate to the use of privileged containers."
   desc  'rationale', "
-    A container running with the `allowPrivilegeEscalation` flag set to `true`
-may have processes that can gain more privileges than their parent.
-
-    There should be at least one PodSecurityPolicy (PSP) defined which does not
-permit containers to allow privilege escalation. The option exists (and is
-defaulted to true) to permit setuid binaries to run.
-
-    If you have need to run containers which use setuid binaries or require
-privilege escalation, this should be defined in a separate PSP and you should
-carefully check RBAC controls to ensure that only limited service accounts and
-users are given permission to access that PSP.
-  "
+A container running with the allowPrivilegeEscalation flag set to true may have processes that can gain more privileges than their parent.
+There should be at least one admission control policy defined which does not permit containers to allow privilege escalation. The option exists (and is defaulted to true) to permit setuid binaries to run.
+If you have need to run containers which use setuid binaries or require privilege escalation, this should be defined in a separate policy and you should carefully check to ensure that only limited service accounts and users are given permission to use that policy.  "
   desc  'check', "
-    Get the set of PSPs with the following command:
-
-    ```
-    kubectl get psp
-    ```
-
-    For each PSP, check whether privileged is enabled:
-
-    ```
-    kubectl get psp <name> -o=jsonpath='{.spec.allowPrivilegeEscalation}'
-    ```
-
-    Verify that there is at least one PSP which does not return true.
+List the policies in use for each namespace in the cluster, ensure that each policy disallows the admission of containers which allow privilege escalation.
   "
-  desc 'fix', "Create a PSP as described in the Kubernetes documentation,
-ensuring that the `.spec.allowPrivilegeEscalation` field is omitted or set to
-false."
+  desc 'fix', "Add policies to each namespace in the cluster which has user workloads to restrict the admission of containers with .spec.allowPrivilegeEscalation set to true."
   impact 0.5
   tag severity: 'medium'
   tag gtitle: nil
